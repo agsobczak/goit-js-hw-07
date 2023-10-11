ed275e60832fee67f6275f86db575cad7b5c8744
agsobczak <s.agasobczak@gmail.com>
2023-10-11 19:35:27 +0200

    Updated3

--- js/task-02.js
+++ js/task-02.js
@@ -1,3 +1,4 @@
+const ingredientList = document.querySelector("#ingredients");
 const ingredients = [
   "Potatoes",
   "Mushrooms",
@@ -7,15 +8,11 @@ const ingredients = [
   "Condiments",
 ];
 
-const ingredientsArr = ingredients.map((ingredient) =>
-  ingredient.toUpperCase()
-);
-
-const ingredientsList = document.querySelector("#ingredients");
-
-for (const ingredient of ingredientsArr) {
+const ingredientsArr = ingredients.map((ingredient) => {
   const li = document.createElement("li");
   li.textContent = ingredient;
   li.classList.add("item");
-  ingredientsList.appendChild(li);
-}
+  return li;
+});
+
+ingredientList.append(...ingredientsArr);
